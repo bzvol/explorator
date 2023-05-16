907ab67e16821e6d138616d841c37669c2b950f8
bzvol <zvolenszki.benjamin@gmail.com>
2023-05-16 14:06:40 +0200

    section 2

--- scripts/index.js
+++ scripts/index.js
@@ -6,3 +6,11 @@ document.querySelectorAll('.header__nav-toggle')
     .forEach((item) => 
         item.addEventListener('click', () => 
             nav.classList.toggle('header__nav-active')));
+
+document.querySelectorAll('.section-images__wrapper')
+    .forEach((item) =>
+        item.addEventListener('click', (e) => {
+            if (item.classList.contains('section-images__wrapper-large') && e.target !== item)
+                return;
+            item.classList.toggle('section-images__wrapper-large');
+        }));
